7c7c78199ca9b64f913ea333ad46021546cd0054
J-Baxter <james.baxter@ed.ac.uk>
2021-12-05 14:01:10 +0000

    problem 2 solved (and function made simpler)

--- scripts/day5.R
+++ scripts/day5.R
@@ -20,29 +20,10 @@ PlotPath <- function(start, finish, map){
   start_y <- start[,'Y1']
   finish_x <- finish[,'X2']
   finish_y <- finish[,'Y2']
-  
-  path_x <- list()
-  path_y <- list()
-  for (i in 1:length(start_x)){
+
+  path_x <- mapply(seq, from = start_x, to = finish_x, SIMPLIFY = F)
+  path_y <- mapply(seq, from = start_y, to = finish_y, SIMPLIFY = F)
     
-    if(start_x[i] == finish_x[i] | start_y[i] == finish_y[i]){
-      path_x[[i]] <- seq(from = start_x[i], to = finish_x[i])
-      path_y[[i]] <- seq(from = start_y[i], to = finish_y[i])
-      
-    }else{
-      path_x[[i]] <- c(seq(from = start_x[i], to = finish_x[i]),
-                       diff(c(start_y[i], finish_y[i])) %>% 
-                         abs() %>% 
-                         rep(finish_x[i],.))
-      
-      path_y[[i]] <- c(diff(c(start_x[i], finish_x[i])) %>% 
-                         abs() %>% 
-                         rep(start_y[i],.),
-                       seq(from = start_y[i], to = finish_y[i]))
-      
-    }
-  }
-  
   map_update <- map
   
   for (i in 1:length(path_x)){
@@ -55,7 +36,7 @@ PlotPath <- function(start, finish, map){
 
 
 # Problem 1
-perp_lines <- lines[which(lines[,'X1'] == lines[,'X2'] | lines[,'Y1'] == lines[,'Y2']),]
+perp_lines <- lines[which(lines[,'X1'] == lines[,'X2'] | lines[,'Y1'] == lines[,'Y2']),] 
 
 start <- cbind('X1' = perp_lines[,'X1'], 'Y1' = perp_lines[,'Y1'])
 finish <- cbind('X2' = perp_lines[,'X2'], 'Y2' = perp_lines[,'Y2'])
@@ -67,3 +48,12 @@ points #7142
 
 
 # Problem 2
+all_lines <- lines +1
+
+start <- cbind('X1' = all_lines[,'X1'], 'Y1' = all_lines[,'Y1'])
+finish <- cbind('X2' = all_lines[,'X2'], 'Y2' = all_lines[,'Y2'])
+
+map_scored <- PlotPath(start = start, finish = finish, map = map_init)
+
+points <- which(map_scored>=2, arr.ind = T) %>% nrow() 
+points #20012
