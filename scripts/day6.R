f97ffa2ccb27a776f283f13de6074d1f89a39d9a
J-Baxter <james.baxter@ed.ac.uk>
2021-12-06 23:35:52 +0000

    problem 2 solved; re-coded to simulate across population rather than individuals

--- scripts/day6.R
+++ scripts/day6.R
@@ -4,49 +4,58 @@ library(tidyverse)
 
 options(scipen = 999)
 
-init_pop <- read.table(file = './data/input_day6', sep = ',') %>%
-  as.numeric()
+# Import 
+init_ages <- read.table(file = './data/input_day6', sep = ',') %>%
+  as.numeric() 
 
-DayGrowth <- function(day_1){
-  next_day <- day_1
-  timer_decline <- which(day_1 %in% c(1,2,3,4,5,6,7,8), arr.ind = T)
-  next_day[timer_decline] <- day_1[which(day_1 %in% c(1,2,3,4,5,6,7,8), arr.ind = T)] -1
+# Initialise population ages as table (matrix)
+# tabulation important due to memory constraints/computational efficiency
+InitPop <- function(population, max_age = 8){
+  null_mat <- matrix(data = 0, ncol = (max_age + 1)) %>% `colnames<-` (c(0:max_age))
+  tab <- table(population) %>% as.matrix() %>% t()
   
-  new_fish <- which(day_1 %in% 0, arr.ind = T)
-  next_day[new_fish] <- 6
+  match <- which(colnames(tab) %in% colnames(null_mat), arr.ind = T) +1
   
-  if(any(day_1 %in% 0)){
-    fish_loc <- which(is.na(next_day), arr.ind = T)[1:length(new_fish)]
-    next_day[fish_loc] <- 8
-  }
+  null_mat[,match] <- tab 
   
-  return(next_day)
-}
-
-
-PopGrowth <- function(gen_0, days){
-  sim_growth <- matrix(data = NA, nrow = days +1, ncol = 500000)
-  sim_growth[1,] <- c(gen_0, rep(NA, 500000 - length(gen_0)))
+  pop_mat <- null_mat
   
-  for(i in 1:days){
-    sim_growth[i+1,] <- DayGrowth(sim_growth[i,])
-  }
-  
-  rownames(sim_growth) <- c(0:days)
-  
-  return(sim_growth)
-  
-}
-
-
-PopSize <- function(pop_mat, gen){
-  pop_per_gen <- apply(pop_mat,1, function(x) x[which(!is.na(x))] %>% length(.))
-  pop_mat <- cbind("days" = 0:gen, 'population_size' = pop_per_gen)
   return(pop_mat)
 }
 
 
+# Update matrix according to problem parameters each day
+UpdatePop <- function(initial, max_days){
+  mat_temp <-  initial
+  days <- 1:max_days
+  
+  for(day in days){
+    mat_updated <-  matrix(data = 0, ncol = ncol(initial)) %>% `colnames<-` (colnames(initial))
+    ageing <- which(colnames(mat_temp) %in% c(1,2,3,4,5,6,7,8), arr.ind = T)
+    spawning <- which(colnames(mat_temp) %in% 0, arr.ind = T)
+    
+    mat_updated[1,(ageing-1)] <- mat_temp[1,ageing]
+    
+    if(mat_temp[,spawning] > 0){
+      
+      mat_updated[1,'8'] <- mat_temp[1,'0']
+      mat_updated[1,'6'] <- mat_updated[1,'6'] + mat_temp[1,'0']
+    }
+    
+    mat_temp <- mat_updated
+  }
+  
+  return(mat_temp)
+}
+
+# Initialise population matrix
+gen_nought <- InitPop(init_ages)
+
 # Problem 1
-eightydays <- PopGrowth(init_pop, 80)
-pop_size <- PopSize(eightydays, 80)
-pop_size[81,] #391671
+eightydays <- UpdatePop(gen_nought, 80)%>% 
+  sum() #391671
+
+
+# Problem 2
+manydays <- UpdatePop(gen_nought, 256)%>% 
+  sum() #1754000560399
