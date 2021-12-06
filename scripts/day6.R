c5c50e22259942c91aef8f78123ed26c44360784
J-Baxter <james.baxter@ed.ac.uk>
2021-12-06 16:15:27 +0000

    import data

--- scripts/day6.R
+++ scripts/day6.R
@@ -3,7 +3,11 @@
 library(tidyverse)
 
 options(scipen = 999)
-ages <- read.table(file = './data/input_day6', sep = ',') %>% 
-  as.numeric()
+
+init_pop <- read.table(file = './data/input_day6', sep = ',') %>%
+  as.numeric() %>%
+  c(., rep(NA, 100000 - length(.))) %>%
+  rbind(.,matrix(data = NA, nrow = 80, ncol = 100000)) %>%
+  `rownames<-` (c(0:80))
 
 # Problem 1
