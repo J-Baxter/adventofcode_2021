7102c851e31220539983a2ae5e031234788c7bd2
J-Baxter <james.baxter@ed.ac.uk>
2021-12-07 15:20:57 +0000

    Problem 1 solved

--- scripts/day7.R
+++ scripts/day7.R
@@ -4,5 +4,13 @@ library(tidyverse)
 options(scipen = 999)
 
 # Import 
+test <- c(16,1,2,0,4,2,7,1,2,14)
+
 init_position <- read.table(file = './data/input_day7', sep = ',') %>%
   as.numeric() 
+
+# Problem 1
+(init_position - median(init_position)) %>%
+  Mod() %>%
+  sum() #347509
+
