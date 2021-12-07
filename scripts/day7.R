4bc269051fa716ca64fe78595d0c807ffca50a2b
J-Baxter <james.baxter@ed.ac.uk>
2021-12-07 16:40:01 +0000

    problem 2 solved

--- scripts/day7.R
+++ scripts/day7.R
@@ -3,14 +3,47 @@
 library(tidyverse)
 options(scipen = 999)
 
-# Import 
-test <- c(16,1,2,0,4,2,7,1,2,14)
 
+# Import 
 init_position <- read.table(file = './data/input_day7', sep = ',') %>%
   as.numeric() 
 
+
+CostAllign <- function(target, position, prob = 1){
+  if (prob == 1){
+    
+    fuel <- abs(position-target) %>% 
+      sum() %>%
+      unlist()
+    
+  }else if(prob == 2){
+    
+    fuel <- abs(position-target) %>% 
+      sapply(.,  seq_len) %>%
+      unlist() %>% 
+      sum()
+    
+  }
+ 
+  return(fuel)
+}
+
+
 # Problem 1
+# Brute Force
+start <- seq(from = min(init_position), to = max(init_position))
+sapply(start, CostAllign, position = init_position, prob = 1) %>% 
+  min() #347509
+
+
+# Knowing that the median is always minimal for step = +1
 (init_position - median(init_position)) %>%
   Mod() %>%
   sum() #347509
 
+
+# Problem 2
+# Brute Force
+start <- seq(from = min(init_position), to = 1000)
+sapply(start, CostAllign, position = init_position, prob = 2) %>% 
+  min() #298257206
